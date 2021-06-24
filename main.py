4e7ec48d8c9d22209150960b7375c5a9a228aac4
Kanhex <ilushareva@gmail.com>
2021-06-24 21:04:13 +0300

    Удалил всё Добавил кода

---
+++ main.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+    print("Five is greater than two!")
+    '''
+    This is a comment
+    written in
+    more that just one line
+    '''
+    carname = 'Volvo'
+    x = 50
+
+y = 10
+x = 5
+z = x + y
+print(z)
