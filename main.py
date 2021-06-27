a1e53d76c5a4b2925ddf7cca3e0354991c348586
Kanhex <ilushareva@gmail.com>
2021-06-27 17:40:48 +0300

    Переделал программу в функцию

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
-"""
-x = int(input())
-y = int(input())
-if x > 8 and y > 8:
-    print(x * y)
-elif x < 8 or y < 8:
-    print(x + y)
-"""
+def a(x, y):
+    if x > 8 and y > 8:
+        print(x * y)
+    elif x < 8 or y < 8:
+        print(x + y)
+    else:
+        print("Error")
 
-'''
-z = int(input())
-x = int(input())
-c = int(input())
-if z > 0 and x > 0 and c > 0:
-    print(z + x + c)
-elif z == 0 and c == 0 or z == 0 and x == 0 or c == 0 and x == 0:
-    print(0)
-elif z < 0 and c < 0 or z < 0 and x < 0 or c < 0 and x < 0:
-    print(z * x * c)
-else:
-    print("Error")
-'''
 
-'''
-a = int(input())
-if 4 < a <= 10:
-    print(2 * a)
-else:
-    print(a - 55)
-'''
-'''
-a = int(input("a = "))
-b = int(input("b = "))
-c = int(input("c = "))
-if a < b:
-    z = a
-else:
-    z = b
-if c < z: 
-    z = c
-x = a + b + c - z
-print(x)
-'''
+a(10, 15)
 
-'''
+
+def b(z, x, c):
+    if z > 0 and x > 0 and c > 0:
+        print(z + x + c)
+    elif z == 0 and c == 0 or z == 0 and x == 0 or c == 0 and x == 0:
+        print(0)
+    elif z < 0 and c < 0 or z < 0 and x < 0 or c < 0 and x < 0:
+        print(z * x * c)
+    else:
+        print("Error")
+
+
+b(4, 6, 2)
+
+
+def c(a):
+    if 4 < a <= 10:
+        print(2 * a)
+    else:
+        print(a - 55)
+
+
+c(6)
+
+
+def d(a,b , c):
+    if a < b:
+        z = a
+    else:
+        z = b
+    if c < z:
+        z = c
+    x = a + b + c - z
+    print(x)
+
+
+d(3, 4, 8)
+
 for Loading in range(8):
     print("Loading...")
-'''
 
-'''
+
 def zxc(count):
     while count != 160:
         print(count)
@@ -55,9 +58,8 @@ def zxc(count):
 
 
 zxc(0)
-'''
 
-'''
+
 def zxc(count):
     while count != 39:
         print(count)
@@ -65,4 +67,3 @@ def zxc(count):
 
 
 zxc(1)
-'''
