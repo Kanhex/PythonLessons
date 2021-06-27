9861405bd2ab51c572d797857d85a20dc0061c23
Kanhex <ilushareva@gmail.com>
2021-06-27 12:35:22 +0300

    27.06 Задачи

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
-mydict = dict(number=int(input()), name=str(input()), season=str(input()))
-x = mydict["season"]
+"""
+x = int(input())
+y = int(input())
+if x > 8 and y > 8:
+    print(x * y)
+elif x < 8 or y < 8:
+    print(x + y)
+"""
+
+'''
+z = int(input())
+x = int(input())
+c = int(input())
+if z > 0 and x > 0 and c > 0:
+    print(z + x + c)
+elif z == 0 and c == 0 or z == 0 and x == 0 or c == 0 and x == 0:
+    print(0)
+elif z < 0 and c < 0 or z < 0 and x < 0 or c < 0 and x < 0:
+    print(z * x * c)
+else:
+    print("Error")
+'''
+
+'''
+a = int(input())
+if 4 < a <= 10:
+    print(2 * a)
+else:
+    print(a - 55)
+'''
+'''
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+if a < b:
+    z = a
+else:
+    z = b
+if c < z: 
+    z = c
+x = a + b + c - z
 print(x)
+'''
+
+'''
+for Loading in range(8):
+    print("Loading...")
+'''
+
+'''
+def zxc(count):
+    while count != 160:
+        print(count)
+        count = count + 10
+
+
+zxc(0)
+'''
+
+'''
+def zxc(count):
+    while count != 39:
+        print(count)
+        count = count + 2
+
+
+zxc(1)
+'''
